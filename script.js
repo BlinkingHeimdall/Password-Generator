e5e2ac01fd23cd972c15d0bdac09fb9e1bd571ac
Jason Adams <jmadams41@gmail.com>
2021-10-03 14:05:11 -0600

    added arrays

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ var uppercase
 var symbols
 var numbers
 
-window.alert("Do you want lowercase letters?")
-window.alert("Do you want upercase letters?")
-window.alert("Would you like to include numbers?")
-window.alert("would you like to include symbols?")
+window.confirm("Do you want lowercase letters?")
+window.confirm("Do you want upercase letters?")
+window.confirm("Would you like to include numbers?")
+window.confirm("would you like to include symbols?")
+
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var symbols = ["!", "@", "#", "$", "%", "&", "*", "/", "<", ">", "?", "+", "-", "_", "="];
 
 
 
