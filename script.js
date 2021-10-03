b093c0f4bca9b42dd90f3e8cbe01f8091e1e27a2
Jason Adams <jmadams41@gmail.com>
2021-10-03 16:40:28 -0600

    notated some stuff

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-var lowercase
-var uppercase
-var symbols
-var numbers
 
-window.confirm("Do you want lowercase letters?")
-window.confirm("Do you want upercase letters?")
-window.confirm("Would you like to include numbers?")
-window.confirm("would you like to include symbols?")
+// variables
+var useLowercase
+var useUppercase
+var useSymbol
+var useNumber
 
+// arrays for characters
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var symbols = ["!", "@", "#", "$", "%", "&", "*", "/", "<", ">", "?", "+", "-", "_", "="];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var symbol = ["!", "@", "#", "$", "%", "&", "*", "/", "<", ">", "?", "+", "-", "_", "="];
 
-
+var parameters
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//questions for parameters
+var useLowercase = window.confirm("Do you want lowercase letters?")
+var useUppercase = window.confirm("Do you want upercase letters?")
+var useNumber = window.confirm("Would you like to include numbers?")
+var useSymbol = window.confirm("Would you like to include symbols?")
+
+passwordLength = Prompt("How many characters, between 8-128, would you like you password to be?")
 
 // Write password to the #password input
 function writePassword() {
