60e04b24a97370c8685b92767a541028f9c04187
Jason Adams <jmadams41@gmail.com>
2022-02-13 18:21:45 -0700

    added logic to check if the password length is a number

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 // variables
+
 var useLowercase
 var useUppercase
 var useSymbol
 var useNumber
 
 // arrays for characters
+
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -14,22 +16,42 @@ var symbol = ["!", "@", "#", "$", "%", "&", "*", "/", "<", ">", "?", "+", "-", "
 var parameters
 
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-//questions for parameters
+//Questions for parameters
+function writePassword() {
 var useLowercase = window.confirm("Do you want lowercase letters?")
-var useUppercase = window.confirm("Do you want upercase letters?")
+var useUppercase = window.confirm("Do you want uppercase letters?")
 var useNumber = window.confirm("Would you like to include numbers?")
 var useSymbol = window.confirm("Would you like to include symbols?")
 
-// a reminder that the user needs to select a parameter
+// A reminder that the user needs to select a parameter to actually get a password
 
 if (!useLowercase && !useNumber && !useSymbol && !useUppercase) {
   window.alert("You need to pick something");
   writePassword();
 }
 
-passwordLength = Prompt("How many characters, between 8-128, would you like you password to be?")
+var PasswordLength = parseInt(window.prompt("How many characters, between 8-24, would you like you password to be?"))
+
+
+
+// checks for a valid number between 8 and 24
+
+while (PasswordLength <8 || PasswordLength >24) {
+  PasswordLength = prompt ("The number of characters for you password has to be between 8 and 24");
+
+}
+
+//double checks the length is a number
+
+if(Number.isNaN(PasswordLength)) {
+  alert("you did not provide a number");
+  return null;
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -39,6 +61,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
